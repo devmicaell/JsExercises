df996ad096a3909553f36ff2a58738d6437b1c3a
devmicaell <157536953+devmicaell@users.noreply.github.com>
2024-02-06 00:13:20 -0300

    Add files via upload

---
+++ VariaveisTipos/script.js
@@ -0,0 +1,5 @@
+var string = "Micael"
+var number = "173"
+var isStudent = true
+
+console.log('Meu nome é: ' + string + '.' + ' Tenho ' + number + ' de altura.' + ' Sou um estudante? ' + isStudent)
